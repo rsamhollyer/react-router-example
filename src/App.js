c2a432de5de5103e33bcba837274a77d3d36d1a8
Sam Hollyer <rsamhollyer@gmail.com>
2021-01-28 15:15:20 -0600

    updated app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,45 @@ import "./App.css";
 import About from "./Components/About";
 import Home from "./Components/Home";
 import Stuff from "./Components/Stuff";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import axios from "axios";
 
 function App() {
+	const [testData, setData] = useState({});
+	const [counter, setCounter] = useState({});
+
+	const getApi = async () => {
+		const response = await axios.get("/api");
+		setData(response.data);
+	};
+
+	useEffect(() => {
+		getApi();
+	}, []);
+
+	// const getValue = async () => {
+	// 	const resp = await axios.get("/api/counter");
+	// 	setCounter(resp.data);
+	// };
+	// const incValue = async () => {
+	// 	const resp = await axios.post("/api/counter");
+	// 	setCounter(resp.data);
+	// };
+	// const decrValue = async () => {
+	// 	const resp = await axios.put("/api/counter");
+	// 	setCounter(resp.data);
+	// };
+	// const resetValue = async () => {
+	// 	const resp = await axios.delete("/api/counter");
+	// 	setCounter(resp.data);
+	// };
+
+	const axiosFnc = async (method) => {
+		const resp = await axios[method]("/api/counter");
+		setCounter(resp.data);
+	};
+
 	return (
 		<Router>
 			<div className="App">
@@ -17,7 +52,12 @@ function App() {
 
 				<Switch>
 					<Route path="/" exact>
-						<Home />
+						<Home testData={testData} />
+						<p>Data is : {counter.value ? counter.value : 0}</p>
+						<button onClick={() => axiosFnc("get")}>Get Value</button>
+						<button onClick={() => axiosFnc("post")}>+</button>
+						<button onClick={() => axiosFnc("put")}>-</button>
+						<button onClick={() => axiosFnc("delete")}>Reset</button>
 					</Route>
 
 					<Route path="/about">
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function Home() {
-	return <h1>Welcome Home</h1>;
+export default function Home(props) {
+	const { testData } = props;
+	return (
+		<>
+			<h1>{testData.status}</h1>
+			<h2>{testData.isCool ? "So COOOOOOLLL" : "TOTALLY NOT KEWL BRO"}</h2>
+
+			{Object.keys(testData).map((key) => {
+				return typeof testData[key] !== "boolean" ? (
+					<p key={testData[key]}>{testData[key]}</p>
+				) : (
+					<p key={1}>SO COOL</p>
+				);
+			})}
+		</>
+	);
 }
