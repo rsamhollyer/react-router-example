a1a4a263ad6c896e6a8184e3af2f0a39fc71799a
Sam Hollyer <rsamhollyer@gmail.com>
2021-01-28 09:55:31 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Stuff from "./Components/Stuff";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
 function App() {
-	return <h1>Hi React</h1>;
+	return (
+		<div className="App">
+			<Home />
+			<About />
+			<Stuff />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/Stuff.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Stuff() {
+	return <h1> Stuff </h1>;
+}
